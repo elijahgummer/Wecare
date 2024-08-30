7f73fef6c8a5de6c776a81c522557ddf23a805bc
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 09:03:05 +1000

    adde soem code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
-function Navbar(){
-    return (
-        <nav>
-
-        </nav>
-    ):
+function Navbar() {
+  return (
+    <nav>
+      <h1>My Navbar</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
