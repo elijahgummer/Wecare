9fba5d98087a10631fa6d12c0b6c75de8db70ab9
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 15:57:51 +1000

    changed the content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ import HeroPic from "./images/hero.png"; // Adjust path as needed
 function Hero() {
   return (
     <div class="container">
+        <h1>We Care About You</h1>
       <img class="logo" src={HeroPic} alt="logo" />
     </div>
   );
