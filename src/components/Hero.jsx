6253377b3ed2c83a0cd895550b81247e04ac4162
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 12:43:15 +1000

    changed the file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './css/Hero.css'; // Importing the CSS file
-import HeroPic from './images/Hero.png'; // Adjust path as needed
+import HeroPic from './images/hero.png'; // Adjust path as needed
 function Hero() {
     return (
         <div class="logo-container">
