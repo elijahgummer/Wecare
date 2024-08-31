e086accce01cc5c4e5b958363e3f3e92c732571b
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 18:42:43 +1000

    changed the buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./css/Hero.css"; // Importing the CSS file
-import { PrimButton, SecButton } from './path/to/Buttons';
+import { PrimButton, SecButton } from './Buttons';
 
 function Hero() {
   return (
@@ -8,6 +8,8 @@ function Hero() {
       <div className="content">
         <h1>We Care About You.</h1>
         <p>Get the care you need, from professionals who are leaders in their fields.</p>
+        <PrimButton />
+        <SecButton />
       </div>
     </div>
   );
