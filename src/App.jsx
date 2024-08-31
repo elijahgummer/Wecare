568bc133909ac1799e40388b17ce2aa1f6d90e27
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 12:42:46 +1000

    imported the hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
