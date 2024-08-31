a32cdf4355c2c96ee5b6f5c682d9ba416742ebcd
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 12:39:30 +1000

    added the hero code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,32 @@
 import React from "react";
 import './css/Hero.css'; // Importing the CSS file
 import Hero from './images/Hero.png'; // Adjust path as needed
+
+function Navbar() {
+    return (
+      <nav class="navbar">
+        <div class="icon phone-btn">
+          <i class="fa-solid fa-phone"></i>
+        </div>
+        <div class="logo-container">
+          <img class="logo" src={logo} alt="logo" />
+        </div>
+        <ul id="sidemenu" class="menu-list">
+          <div class="icon cancel-btn">
+            <i class="fa-solid fa-xmark"></i>
+          </div>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="articles.html">Articles</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <button>CALL NOW</button>
+        </ul>
+        <div class="icon menu-btn">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+      </nav>
+    );
+  }
+  export default Hero;
+  
