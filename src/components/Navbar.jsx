11ec8bbf81135f4824695e3156b9ff56a3e112d8
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 12:28:13 +1000

    deleted some whitespace

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import './css/Navbar.css'; // Importing the CSS file
 import logo from './images/logo.png'; // Adjust path as needed
-
 function Navbar() {
   return (
     <nav class="navbar">
