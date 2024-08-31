3528011c141befe9e21eb58c96574749c8b45df2
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 16:03:32 +1000

    changed the class to classname

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import './css/Navbar.css'; // Importing the CSS file
 import logo from './images/logo.png'; // Adjust path as needed
 function Navbar() {
   return (
-    <nav class="navbar">
+    <nav className="navbar">
       <div className="icon phone-btn">
         <i className="fa-solid fa-phone"></i>
       </div>
       <div className="logo-container">
         <img className="logo" src={logo} alt="logo" />
       </div>
-      <ul id="sidemenu" class="menu-list">
-        <div class="icon cancel-btn">
-          <i class="fa-solid fa-xmark"></i>
+      <ul id="sidemenu" className="menu-list">
+        <div className="icon cancel-btn">
+          <i className="fa-solid fa-xmark"></i>
         </div>
         <li><a href="index.html">Home</a></li>
         <li><a href="services.html">Services</a></li>
@@ -21,8 +21,8 @@ function Navbar() {
         <li><a href="contact.html">Contact</a></li>
         <button>CALL NOW</button>
       </ul>
-      <div class="icon menu-btn">
-        <i class="fa-solid fa-bars"></i>
+      <div className="icon menu-btn">
+        <i className="fa-solid fa-bars"></i>
       </div>
     </nav>
   );
