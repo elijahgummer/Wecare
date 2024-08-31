b9a6bdbcec38096bfe2d1c5225044a5af04f87dd
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 12:13:18 +1000

    added the logo image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './css/Navbar.css'; // Importing the CSS file
+import logo from './images/example.jpg'; // Adjust path as needed
 
 function Navbar() {
   return (
@@ -8,7 +9,7 @@ function Navbar() {
         <i class="fa-solid fa-phone"></i>
       </div>
       <div class="logo-container">
-        <img class="logo" src="./images/logo.png" alt="logo" />
+        <img class="logo" src={logo} alt="logo" />
       </div>
       <ul id="sidemenu" class="menu-list">
         <div class="icon cancel-btn">
