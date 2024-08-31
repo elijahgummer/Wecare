b647a6b047a5708077bbdad4f38e504bbb90aa8f
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 12:28:39 +1000

    deleted some whitespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import Navbar from './components/Navbar';
-
 function App() {
   return (
     <div className="App">
@@ -8,5 +7,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
