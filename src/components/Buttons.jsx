aae30b9eba91a7587e925568e5eb7484c5248d31
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 18:38:26 +1000

    added the buttons

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./css/Buttons.css"; // Importing the CSS file
+
+function primButton() {
+    return(
+        <button className="primButton"></button>
+    );
+};
+
+export default primButton;
--- src/components/css/Buttons.jsx
+++
@@ -1,2 +0,0 @@
-import React from "react";
-import "./css/Hero.css"; // Importing the CSS file
