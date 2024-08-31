b38574008257ef8cd5b47f2a6997067cc472206b
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 16:05:00 +1000

    added the description for the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,7 @@ function Hero() {
     <div className="container">
         <div className="content">
         <h1>We Care About You</h1>
+        <p>Get the care you need, from professionals who are leaders in their fields. </p>
         </div>
       <img className="logo" src={HeroPic} alt="logo" />
     </div>
