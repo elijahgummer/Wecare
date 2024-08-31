1644270c18fb8ba9f5fb11007310008b7192d562
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 16:07:59 +1000

    added another div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,14 @@ import "./css/Hero.css"; // Importing the CSS file
 import HeroPic from "./images/hero.png"; // Adjust path as needed
 function Hero() {
   return (
+    <div id="hero">
     <div className="container">
         <div className="content">
         <h1>We Care About You</h1>
         <p>Get the care you need, from professionals who are leaders in their fields. </p>
         </div>
       <img className="logo" src={HeroPic} alt="logo" />
+    </div>
     </div>
   );
 }
