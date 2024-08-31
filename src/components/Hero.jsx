8b4df2fd9315003fd6c2720781c7c7bf5061896c
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 12:43:45 +1000

    reformatted the area

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import './css/Hero.css'; // Importing the CSS file
-import HeroPic from './images/hero.png'; // Adjust path as needed
+import "./css/Hero.css"; // Importing the CSS file
+import HeroPic from "./images/hero.png"; // Adjust path as needed
 function Hero() {
-    return (
-        <div class="logo-container">
-        <img class="logo" src={HeroPic} alt="logo" />
-      </div>
-    );
-  }
+  return (
+    <div class="logo-container">
+      <img class="logo" src={HeroPic} alt="logo" />
+    </div>
+  );
+}
 export default Hero;
-  
