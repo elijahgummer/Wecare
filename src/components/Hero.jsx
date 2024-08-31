115dacdfe1517e3aafb9b4618670078c2d60cfdc
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-31 17:41:57 +1000

    changed the text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div id="hero">
       <div className="content">
-        <h1>We Care About You</h1>
+        <h1>We Care About You.</h1>
         <p>Get the care you need, from professionals who are leaders in their fields.</p>
       </div>
     </div>
