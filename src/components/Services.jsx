8ecd34b0e85ff03f3e0914154da2da5620b8de31
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 14:08:34 +1000

    changed the code

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,7 +7,7 @@ function Services() {
         <div id="services">
             <div className="content">
                 <h1>Our services</h1>
-                <div className="image-container">
+                <div className="images-container">
                     <img src={obstetrics} alt="obstetrics" />
                 </div>
             </div>
