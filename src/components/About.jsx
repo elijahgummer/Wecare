2997f56e4ddc5fcbf00f7ac1b56cae3ec32dea42
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 12:06:11 +1000

    changed the code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './css/About.css'; // Importing the CSS file
+import { PrimButton } from "./Buttons";
 
 function About() {
     return(
@@ -8,6 +9,7 @@ function About() {
                 <div className="content">
                     <h1>The Clinic</h1>
                     <p>We Care Medical Clinic was founded in 1973 by a group of doctors who felt the need for a holistic clinic in the community, one that would serve a wide variety of medical needs. The clinic opened in a large Victorian style home and has grown, over time, to include more than 70 doctors and specialists.</p>
+                    <SecButton>LEARN MORE</SecButton>
                 </div>
             </div>
         </div>
