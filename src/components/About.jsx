aaf603c0a5c158e7c21575e789ae31766f532dc2
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 12:12:23 +1000

    added an image container

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,9 @@ function About() {
     return(
         <div id="about">
             <div className="container">
+                <div className="image-container">
+                    
+                </div>
                 <div className="content">
                     <h1>The Clinic</h1>
                     <p>We Care Medical Clinic was founded in 1973 by a group of doctors who felt the need for a holistic clinic in the community, one that would serve a wide variety of medical needs. The clinic opened in a large Victorian style home and has grown, over time, to include more than 70 doctors and specialists.</p>
