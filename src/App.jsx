1918c1257ac051ab57ecd0f0eba23a2e31198a3f
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 14:09:49 +1000

    imported the services component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import Services from './components/Services';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <About /> 
+      <Services />
     </div>
   );
 }
