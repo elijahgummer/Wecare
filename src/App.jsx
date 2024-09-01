ce1e41ca33926522badddd236db8a9d9107fb1a0
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 15:57:46 +1000

    added the cta component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
+import CTA from './components/CTA';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About /> 
       <Services />
+      <CTA />
     </div>
   );
 }
