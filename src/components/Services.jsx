41ca872dd1e3786c979a447414137f6d654a4add
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 14:05:22 +1000

    added the things

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,6 +3,10 @@ import './css/Services.css';
 
 function Services() {
     return(
-        
+        <div id="services">
+            <div className="content">
+                <h1>Our services</h1>
+            </div>
+        </div>
     );
 }
