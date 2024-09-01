4eee6bccedb2b8ec283e5ff2948c49d537b7a39b
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 14:13:50 +1000

    reformated the code

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import './css/Services.css';
-import obstetrics from './images/obstetrics.png'
-import pediatrics from './images/pediatrics.png'
-import seniorcare from './images/seniorcare.png'
+import "./css/Services.css";
+import obstetrics from "./images/obstetrics.png";
+import pediatrics from "./images/pediatrics.png";
+import seniorcare from "./images/seniorcare.png";
 
 function Services() {
-    return(
-        <div id="services">
-            <div className="content">
-                <h1>Our services</h1>
-                <div className="images-container">
-                    <div className="image-box">
-                    <img src={obstetrics} alt="obstetrics" />
-                    </div>
-                    <div className="image-box">
-                    <img src={pediatrics} alt="pediatrics" />
-                    </div>
-                    <div className="image-box">
-                    <img src={seniorcare} alt="seniorcare" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id="services">
+      <div className="content">
+        <h1>Our services</h1>
+        <div className="images-container">
+          <div className="image-box">
+            <img src={obstetrics} alt="obstetrics" />
+          </div>
+          <div className="image-box">
+            <img src={pediatrics} alt="pediatrics" />
+          </div>
+          <div className="image-box">
+            <img src={seniorcare} alt="seniorcare" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 export default Services;
