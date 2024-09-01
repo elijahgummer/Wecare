4a0b064b2fe28c7e69a7c4b77477fbe3f0ae4ffc
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 15:24:25 +1000

    changed the code

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -12,24 +12,38 @@ function Services() {
         <div className="images-container">
           <div className="image-box">
             <img src={obstetrics} alt="obstetrics" />
-          </div>
-          <div className="content-box">
-            <h1>OBSTETRICS</h1>
-            <p>Access a complete line of medical services to maintain your health throughout pregnancy, including evaluation, diagnosis and treatment.</p>
+
+            <div className="content-box">
+              <h1>OBSTETRICS</h1>
+              <p>
+                Access a complete line of medical services to maintain your
+                health throughout pregnancy, including evaluation, diagnosis and
+                treatment.
+              </p>
+            </div>
           </div>
           <div className="image-box">
             <img src={pediatrics} alt="pediatrics" />
             <div className="content-box">
               <h1>PEDIATRICS</h1>
-              <p>With four in-house pediatric doctors, each one a specialist in the field, we ensure that every child at the clinic gets the care they require.</p>
+              <p>
+                With four in-house pediatric doctors, each one a specialist in
+                the field, we ensure that every child at the clinic gets the
+                care they require.
+              </p>
             </div>
           </div>
           <div className="image-box">
             <img src={seniorcare} alt="seniorcare" />
-          </div>
-          <div className="content-box">
-            <h1>SENIOR CARE</h1>
-            <p>We offer comprehensive primary care and consultations to support patients and families cope with the challenging conditions related to aging.</p>
+
+            <div className="content-box">
+              <h1>SENIOR CARE</h1>
+              <p>
+                We offer comprehensive primary care and consultations to support
+                patients and families cope with the challenging conditions
+                related to aging.
+              </p>
+            </div>
           </div>
         </div>
       </div>
