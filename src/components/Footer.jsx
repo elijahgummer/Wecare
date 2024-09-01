e7e47c452ee2f1f4531e471faeb4d4f68a652b4d
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 19:03:22 +1000

    changed the code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,19 @@ import "./css/Footer.css";
 
 function Footer() {
   return (
-      <footer>
-        <div class="container">
-          <div class="content">
-            <div class="info-box">
-              <i class="fa-solid fa-phone"></i>
-              <h3>PHONE</h3>
-              <p>1-234-567-890</p>
-            </div>
-            <div class="info-box">
-              <i class="fa-solid fa-location-dot"></i>
-              <h3>ADDRESS</h3>
-              <p>We Care | Medical Clinic</p>
-              <div class="logo-container">
+    <footer>
+      <div class="container">
+        <div class="content">
+          <div class="info-box">
+            <i class="fa-regular fa-comments"></i>
+            <h3>CONTACT</h3>
+            <p>1-234-567-890</p>
+          </div>
+          <div class="info-box">
+            <i class="fa-solid fa-location-dot"></i>
+            <h3>ADDRESS</h3>
+            <p>We Care | Medical Clinic</p>
+            <div class="logo-container">
               <div class="social-icons">
                 <i class="fa-brands fa-facebook"></i>
                 <i class="fa-brands fa-google-plus-g"></i>
@@ -23,15 +23,15 @@ function Footer() {
                 <i class="fa-brands fa-instagram"></i>
               </div>
             </div>
-            </div>
-            <div class="info-box">
-              <i class="fa-regular fa-envelope"></i>
-              <h3>EMAIL</h3>
-              <p>example@gmail.com</p>
-            </div>
+          </div>
+          <div class="info-box">
+            <i class="fa-regular fa-envelope"></i>
+            <h3>EMAIL</h3>
+            <p>example@gmail.com</p>
           </div>
         </div>
-      </footer>
+      </div>
+    </footer>
   );
 }
 
