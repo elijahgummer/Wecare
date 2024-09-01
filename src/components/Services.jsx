a0fc2e80af0160b8ebdf8941663a0d5a8379b424
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 14:07:30 +1000

    changed the code

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import './css/Services.css';
+import obstetrics from './imgages/obstetrics.png'
 
 function Services() {
     return(
         <div id="services">
             <div className="content">
                 <h1>Our services</h1>
+                <div className="image-container">
+                    <img src={obstetrics} alt="obstetrics" />
+                </div>
             </div>
         </div>
     );
