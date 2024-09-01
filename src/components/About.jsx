db248fcfb15e7b363fc90df526915d4c78d59c77
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-01 12:10:14 +1000

    updated the button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './css/About.css'; // Importing the CSS file
-import { SecButton } from "./Buttons";
+import { ThirdButton } from "./Buttons";
 
 function About() {
     return(
@@ -9,7 +9,7 @@ function About() {
                 <div className="content">
                     <h1>The Clinic</h1>
                     <p>We Care Medical Clinic was founded in 1973 by a group of doctors who felt the need for a holistic clinic in the community, one that would serve a wide variety of medical needs. The clinic opened in a large Victorian style home and has grown, over time, to include more than 70 doctors and specialists.</p>
-                    <SecButton>LEARN MORE</SecButton>
+                    <ThirdButtonButton>LEARN MORE</ThirdButtonButton>
                 </div>
             </div>
         </div>
