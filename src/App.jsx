750ae9da5d9e6fdedb343995d06f3f37e95e8a43
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-04 18:06:37 +1000

    added teh code

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,52 @@ import Services from './components/Services';
 import CallToAction from './components/callToAction';
 import Footer from './components/Footer';
 import React, { useState } from 'react';
-function App() {
+
+// Example Page Components
+function HomePage() {
   return (
-    const [currentPage, setCurrentPage] = useState('home');
-    <div className="App">
-      <Navbar />
+    <div>
       <Hero />
-      <About /> 
+      <About />
       <Services />
       <CallToAction />
+    </div>
+  );
+}
+
+function AboutPage() {
+  return (
+    <div>
+      <About />
+    </div>
+  );
+}
+
+function NotFoundPage() {
+  return <div><h1>404 - Page Not Found</h1></div>;
+}
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <HomePage />;
+      case 'about':
+        return <AboutPage />;
+      default:
+        return <NotFoundPage />;
+    }
+  };
+
+  return (
+    <div className="App">
+      <Navbar setCurrentPage={setCurrentPage} />
+      {renderPage()}
       <Footer />
     </div>
   );
 }
+
 export default App;
