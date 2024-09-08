5b41855e7f8847e2ed295e57deb634a7da7790e9
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-08 19:44:34 +1000

    changed the code

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,5 @@
 // ServicesPage.js
 import React from 'react';
-import Footer from '../components/Footer'; // Adjust the import path as needed
 function ServicesPage() {
   return (
     <div id="servicePage">
