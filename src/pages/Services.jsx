b102a35b19e5a43f5baea626a2769951cc96b841
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-08 19:52:06 +1000

    added the hero container with the image

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,13 +1,13 @@
 // ServicesPage.js
-import React from 'react';
-import logo from './images/logo.png'; // Adjust path as needed
+import React from "react";
+import logo from "./images/logo.png"; // Adjust path as needed
 function ServicesPage() {
   return (
     <div id="servicePage">
-              <img className="logo" src={logo} alt="logo" />
-
+      <div className="hero-con">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
     </div>
   );
 }
 export default ServicesPage;
-
