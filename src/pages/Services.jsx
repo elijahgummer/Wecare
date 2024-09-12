9315e0567214f5f7beecb47272d2cf914291547d
Elijah Gummer <elijahgummer5@gmail.com>
2024-09-12 19:16:13 +1000

    addded the class

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,11 +1,12 @@
 // ServicesPage.js
 import React from "react";
 import servicesHeader from "../components/images/servicesPageHeader.png"; // Adjust path as needed
+
 function ServicesPage() {
   return (
     <div id="servicePage">
       <div className="hero-con">
-        <img className="logo" src={servicesHeader} alt="logo" />
+        <img className="servicesHeader" src={servicesHeader} alt="logo" />
       </div>
     </div>
   );
