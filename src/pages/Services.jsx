8705612536f04fa5f0941ee12f8b44c82e2331c8
Elijah Gummer <elijahgummer5@gmail.com>
2024-10-14 12:09:39 +1000

    changed the code

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,6 +8,7 @@ function ServicesPage() {
       <div className="hero-con">
         <img className="servicesHeader" src={servicesHeader} alt="logo" />
       </div>
+      <p>We offer a wide range of services, from maternal care to senior care, from pediatrics to dentistry. Our mission is to help you maintain a high quality of life through best health practices, and our entire staff is dedicated to this goal. We offer holistic solutions to meet all of your health needs. </p>
     </div>
   );
 }
