098d6fe9e1c74164801864a0cdfa97229e8a3af3
Elijah Gummer <elijahgummer5@gmail.com>
2024-10-14 16:35:14 +1000

    changed the code

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,8 +4,8 @@ import "../components/css/servicesPage.css";
 import obstetrics from "../components/images/obstetrics.png";
 import pediatrics from "../components/images/pediatrics.png";
 import seniorcare from "../components/images/seniorcare.png";
-import pills from './images/pills.png'
-import { PrimButton } from "./Buttons";
+import pills from '../components/images/pills.png'
+import { PrimButton } from "../components/Buttons";
 function ServicesPage() {
   return (
     <div id="servicePage">
