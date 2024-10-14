722b5117d2240668817616c61eb312bf9afc35a1
Elijah Gummer <elijahgummer5@gmail.com>
2024-10-14 13:32:51 +1000

    added the content

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,9 +2,9 @@
 import React from "react";
 import servicesHeader from "../components/images/servicesPageHeader.png"; // Adjust path as needed
 import "../components/css/servicesPage.css";
-import obstetrics from "./images/obstetrics.png";
-import pediatrics from "./images/pediatrics.png";
-import seniorcare from "./images/seniorcare.png";
+import obstetrics from "../components/images/obstetrics.png";
+import pediatrics from "../components/images/pediatrics.png";
+import seniorcare from "../components/images/seniorcare.png";
 function ServicesPage() {
   return (
     <div id="servicePage">
@@ -20,6 +20,8 @@ function ServicesPage() {
       </p>
       <div className="card-container">
         <img src={obstetrics} alt="obstetrics" />
+        <h1>Pediatrics</h1>
+        <p>From newborns until the age of 18, pediatric medicine covers everything from vaccinations and checkups to sleeping and feeding problems, obesity, learning issues and mental health. We have four in-house pediatric doctors, each with his or her own specialty, to ensure that you get the pediatric care that every child deserves. </p>
       </div>
       <div className="card-container">
         <img src={pediatrics} alt="pediatrics" />
