c7bf6af76ae3650fd1d6b2257853fe82e8188b1a
Elijah Gummer <elijahgummer5@gmail.com>
2024-10-14 14:05:21 +1000

    changed the code

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -19,24 +19,44 @@ function ServicesPage() {
         health needs.{" "}
       </p>
       <div className="card-container">
-        <img src={obstetrics} alt="obstetrics" />
+        <img src={pediatrics} alt="pediatrics" />
         <div className="content">
-        <h1>Pediatrics</h1>
-        <p>From newborns until the age of 18, pediatric medicine covers everything from vaccinations and checkups to sleeping and feeding problems, obesity, learning issues and mental health. We have four in-house pediatric doctors, each with his or her own specialty, to ensure that you get the pediatric care that every child deserves. </p>
+          <h1>Pediatrics</h1>
+          <p>
+            From newborns until the age of 18, pediatric medicine covers
+            everything from vaccinations and checkups to sleeping and feeding
+            problems, obesity, learning issues and mental health. We have four
+            in-house pediatric doctors, each with his or her own specialty, to
+            ensure that you get the pediatric care that every child deserves.{" "}
+          </p>
         </div>
       </div>
       <div className="card-container">
-        <img src={pediatrics} alt="pediatrics" />
+        <img src={obstetrics} alt="obstetrics" />
         <div className="content">
           <h1>Obstetrics </h1>
-          <p>We provide a complete line of medical services to ensure a safe and sound pregnancy. The services we offer cover personalized care before, during and after pregnancy. Our in-house midwives and obstetricians provide periodic comprehensive examinations and childbirth preparation classes. We also offer evaluation, diagnosis and treatment of a wide range of obstetric-related issues.</p>
+          <p>
+            We provide a complete line of medical services to ensure a safe and
+            sound pregnancy. The services we offer cover personalized care
+            before, during and after pregnancy. Our in-house midwives and
+            obstetricians provide periodic comprehensive examinations and
+            childbirth preparation classes. We also offer evaluation, diagnosis
+            and treatment of a wide range of obstetric-related issues.
+          </p>
         </div>
       </div>
       <div className="card-container">
         <img src={seniorcare} alt="seniorcare" />
         <div className="content">
           <h1>Senior Care</h1>
-          <p>Our team of geriatric specialists helps you manage your changing health needs as you age. We offer comprehensive primary care and consultations to support patients and families coping with challenging conditions related to aging. Our services include primary care for adults 65 years and older who have neurological conditions such as dementia and cognitive impairment. </p>
+          <p>
+            Our team of geriatric specialists helps you manage your changing
+            health needs as you age. We offer comprehensive primary care and
+            consultations to support patients and families coping with
+            challenging conditions related to aging. Our services include
+            primary care for adults 65 years and older who have neurological
+            conditions such as dementia and cognitive impairment.{" "}
+          </p>
         </div>
       </div>
     </div>
