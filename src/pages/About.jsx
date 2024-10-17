e0957bb9f23883beed0c8385a8afbc8b6b928e10
Elijah Gummer <elijahgummer5@gmail.com>
2024-10-17 18:49:07 +1000

    added the paragrahp

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,13 @@ function AboutPage() {
         <div className="image-container">
           <img className="people-image" src={people} alt="people-working" />
         </div>
-        <p></p>
+        <p>We Care Medical Clinic is dedicated to optimizing the quality of healthcare through an integrative approach to health and wellness. We include both conventional and complementary treatments for general health, healing and well being. 
+
+
+
+Our goal is to model and advance a standard of integrative health care. We achieve this goal through our four core values: integration, compassion, expertise and time. Each and every professional that provides services at our clinic - from nurses and doctors to technicians and administrative staff, are dedicated to your good health and wellness. We do our best at the clinic so you live a full and healthy life, and spend as little time as possible at the clinic.
+
+</p>
       </div>
     </div>
   );
