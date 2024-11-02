4f9a067789a1f0a4068df4c5b580ef90564fc2f3
Elijah Gummer <elijahgummer5@gmail.com>
2024-11-02 20:35:31 +1000

    changed the code

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import servicesHeader from "../components/images/servicesPageHeader.png"; // Adjust path as needed
 import "../components/css/contactPage.css";
-import aboutHeader from './images/logo.png'; // Adjust path as needed
+import contactHeader from './images/contactHeader.png'; // Adjust path as needed
 function ContactPage() {
   return (
     <div id="contactPage">
