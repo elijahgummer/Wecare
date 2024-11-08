f4dca57417b9e9635e7b47820de328ee526d5dc8
Elijah Gummer <elijahgummer5@gmail.com>
2024-11-08 10:16:51 +1000

    changed the font wieght

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ function ContactPage() {
         <img className="contactHeader" src={contactHeader} alt="logo" />
         <h1>CONTACT</h1>
       </div>
-      <h2>Send<span>Us A Message</span></h2>
+      <h2>Send<span>Us a Message</span></h2>
       <p>Have a question? We Care Medical Clinic is here to help. Send us a message and we’ll be in touch. </p>
     </div>
   );
