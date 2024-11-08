e000612932c59eb52d4463d44f938c7bb6e5cee6
Elijah Gummer <elijahgummer5@gmail.com>
2024-11-08 10:10:46 +1000

    changed rhe code

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../components/css/contactPage.css";
-import contactHeader from './images/contactHeader.png'; // Adjust path as needed
+import contactHeader from '../components/images/contactHeader.png'; // Adjust path as needed
 function ContactPage() {
   return (
     <div id="contactPage">
