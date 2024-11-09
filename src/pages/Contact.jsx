ec65965aa87f88ad3b3e125dbe3370598eabf258
Elijah Gummer <elijahgummer5@gmail.com>
2024-11-09 19:18:33 +1000

    changed the code

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,7 +19,6 @@ function ContactPage() {
 
           <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" required />
-
           <label htmlFor="message">Message</label>
           <textarea id="message" name="message" required></textarea>
           <button type="submit">Send Message</button>
