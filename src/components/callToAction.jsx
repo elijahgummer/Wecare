27f644707b72445d9442902b9db6dc2c5d88038d
Elijah Gummer <elijahgummer5@gmail.com>
2024-11-23 20:42:35 +1000

    changedd the cta section

--- src/components/callToAction.jsx
+++ src/components/callToAction.jsx
@@ -1,21 +1,51 @@
 import React from "react";
-import './css/CTA.css'
-import pills from './images/pills.png'
+import Slider from "react-slick"; // Import the react-slick carousel
+import "slick-carousel/slick/slick.css"; // Slick CSS
+import "slick-carousel/slick/slick-theme.css"; // Slick theme CSS
+import "./css/CTA.css";
+import pills1 from "./images/pills1.png";
+import pills2 from "./images/pills2.png";
+import pills3 from "./images/pills3.png";
 import { PrimButton } from "./Buttons";
+
 function CallToAction() {
-    return(
-        <div id="cta">
-            <div className="container">
-                <div className="image-container">
-                    <img src={pills} alt="pills" />
-                </div>
-                <div className="content">
-                    <h1>Public Pharmacy</h1>
-                    <p>Managing your medications at We Care Medical Clinic is simple, from start to finish.                     </p>
-                    <PrimButton>LEARN MORE</PrimButton>
-                </div>
+  const sliderSettings = {
+    dots: true, // Enable dots for navigation
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true, // Auto-play feature
+    autoplaySpeed: 3000, // Delay between slides
+  };
+
+  return (
+    <div id="cta">
+      <div className="container">
+        <div className="image-container">
+          <Slider {...sliderSettings}>
+            <div>
+              <img src={pills1} alt="Pills 1" />
             </div>
+            <div>
+              <img src={pills2} alt="Pills 2" />
+            </div>
+            <div>
+              <img src={pills3} alt="Pills 3" />
+            </div>
+          </Slider>
         </div>
-    );
+        <div className="content">
+          <h1>Public Pharmacy</h1>
+          <p>
+            Managing your medications at We Care Medical Clinic is simple, from
+            start to finish.
+          </p>
+          <PrimButton>LEARN MORE</PrimButton>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default CallToAction;
